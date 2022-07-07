0f837223f41f1530cc711680359c6c4f7e3a59c3
manuks-22 <manu_k_s@hotmail.com>
2022-07-07 09:36:55 +0530

    Added logging to error handling controller.

--- CryptoCurrencyViewApp/Controllers/ErrorController.cs
+++ CryptoCurrencyViewApp/Controllers/ErrorController.cs
@@ -1,11 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CryptoCurrencyApp.Infrastructure.Logging;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CryptoCurrencyApp.Controllers
 {
     [ApiController]
     public class ErrorController : ControllerBase
-    {
+    { 
+        private readonly ILogManager _logger;
+
+        public ErrorController(ILogManager logger)
+        {
+            _logger = logger; 
+        }
+
         [Route("error")]
-        public IActionResult Error() => Problem();
+        public IActionResult Error()
+        {
+            var exception = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            _logger.Error(@$"Error in API | {exception.Error.Message}| {exception.Error.Sta}" );
+            return Problem();
+        }
+       
     }
 }
